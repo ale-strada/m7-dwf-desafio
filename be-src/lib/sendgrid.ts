b5d506f541350ccb3a2542f22acc7357b155e222
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-08-09 18:56:27 -0300

    cambio lib sengrid

--- be-src/lib/sendgrid.ts
+++ be-src/lib/sendgrid.ts
@@ -1,7 +1,5 @@
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(
-  "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"
-);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export { sgMail };
