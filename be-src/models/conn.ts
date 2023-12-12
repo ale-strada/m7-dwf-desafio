f0f111d90398f5b547343b7eaeaa46642a59fd33
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-12-12 15:31:34 -0300

    cambio de DB postgres

--- be-src/models/conn.ts
+++ be-src/models/conn.ts
@@ -1,20 +1,20 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "pet_finder_db_user",
-  password: process.env.SECUALIZE_PASS,
-  database: "pet_finder_db",
-  port: 5432,
-  host: "dpg-cdo3lsirrk05dt2gmdeg-a.oregon-postgres.render.com",
-  ssl: true,
-  // esto es necesario para que corra correctamente
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+	dialect: "postgres",
+	username: "qalwatfi",
+	password: process.env.SECUALIZE_PASS,
+	database: "qalwatfi",
+	port: 5432,
+	host: "silly.db.elephantsql.com",
+	ssl: true,
+	// esto es necesario para que corra correctamente
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 // hacer push y cambiar clave en env vars
